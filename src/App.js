90f8ab60db67b3857b92de27c4cdc25b6409b04c
unknown <Yazh0001@student.monash.edu>
2023-09-16 22:02:44 +0400

    removed about

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Contact from './Components/Contact'
 import Projects from './Components/Projects'
-import Aboutme from './Components/Aboutme'
+// import Aboutme from './Components/Aboutme'
 
 const App = () =>(
   <div>
     <Navbar/>
     <Hero/>
     <Projects/>
-    <Aboutme/>
+    {/* <Aboutme/> */}
     <Contact/>
   </div>
   
